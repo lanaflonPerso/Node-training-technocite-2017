77d416fc91f49a2a257c6fe310eae9ce4c9cdfd9
Gilles BERTRAND <gilles@triptyk.eu>
2017-11-22 19:58:30 +0100

    add exercice10 review

---
+++ exercice10/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+const promisify = require('es6-promisify');
+const routes = require('./routes/index');
+const helpers = require('./helpers');
+const errorHandlers = require('./handlers/errorHandlers');
+
+// create our Express app
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'handlebars');
+
+// serves up static files from the public folder. Anything in public/ will just be served up as the file it is
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+
+// If that above routes didnt work, we 404 them and forward to error handler
+app.use(errorHandlers.notFound);
+
+// Otherwise this was a really bad error we didn't expect! Shoot eh
+if (app.get('env') === 'development') {
+    /* Development Error Handler - Prints stack trace */
+    app.use(errorHandlers.developmentErrors);
+}
+
+// production error handler
+app.use(errorHandlers.productionErrors);
+
+// done! we export it so we can start the site in start.js
+module.exports = app;
