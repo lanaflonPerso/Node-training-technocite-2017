3e4b195e18cb1ddbd15ba5661827b9b20662dbdf
9073_nodejs (Teacher) <t_9073_nodejs@PEDAGOGIQUE.LAN>
2017-11-22 11:49:11 +0100

    Add step 4

--- exercice07/controllers/api/books.js
+++ exercice07/controllers/api/books.js
@@ -10,19 +10,22 @@ module.exports = (req, res) => {
                 })
 
             break;
+
             case 'POST':
             let formBook='';
             req.on('data',((data)=>{
                 formBook+=data;
             }));
             req.on('end',()=>{
-                console.log(JSON.parse(formBook))
+                book.addBook(JSON.parse(formBook)).then(()=>{
+                    book.getAllBooks()
+                    .then((data) => {
+                        res.setHeader('Content-type', 'application/json')
+                        res.end(JSON.stringify(data.toString('utf8')))
+                    })
+               })
             })
            
-            // book.addBook('values')
-            res.setHeader('Content-type', 'application/json')
-
-            res.end();
         default:
             break
     }
--- exercice07/models/books.js
+++ exercice07/models/books.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const promisify = require("es6-promisify");
 const read = promisify(fs.readFile);
+const write = promisify(fs.writeFile);
 let books =null;
 module.exports=class BookModel {
     constructor(){
@@ -9,5 +10,13 @@ module.exports=class BookModel {
     }
     getAllBooks(){
         return read(`${process.cwd()}/models/books.json`)
-    } 
+    }
+    addBook(obj){
+        return read(`${process.cwd()}/models/books.json`).then((data)=>{
+            var arr = JSON.parse(data)
+            arr.push(obj)
+            return write(`${process.cwd()}/models/books.json`,JSON.stringify(arr))
+        })
+        
+    }
 }
